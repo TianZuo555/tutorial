6de877a8bc5a5d3ca4499d8e028ed30e79d6402f
Tian Zuo <tian.zuo@Perficient.com>
2022-06-01 15:17:04 +0800

    2 category component

--- src/components/category/Category.component.jsx
+++ src/components/category/Category.component.jsx
@@ -1,7 +1,12 @@
+import './category.component.scss';
+
 export const Category = (props) => {
-    const { title, id } = props.category;
+    const { title, id, imageUrl } = props.category;
     return (
         <div className='category-container' key={id}>
+        <div className='background-image' style={{
+            'backgroundImage': `url(${imageUrl})`
+        }}></div>
             <div className='category-body-container'>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
