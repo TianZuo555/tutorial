f8e30fd71bd2162221dacb3ff6e612eb5d8b406a
Tian Zuo <tian.zuo@Perficient.com>
2022-06-24 22:05:44 +0800

    style form

---
+++ src/components/formInput/FormInput.component.jsx
@@ -0,0 +1,10 @@
+import './form-input.styles.scss';
+
+export default function FormInput({ label, ...otherProps }) {
+    return (
+        <div className='group'>
+            <label className={`${otherProps.value.length ? 'shrink' : ''}  form-input-label`}>{label}</label>
+            <input className='form-input' {...otherProps}></input>
+        </div>
+    );
+}
--- src/components/signup/SignUp.component.jsx
+++ src/components/signup/SignUp.component.jsx
@@ -1,3 +1,4 @@
+import FormInput from '../formInput/FormInput.component';
 import { postByApiEndpoint } from '../../apiUtils/apiUtils';
 import { useState } from 'react';
 import { userEndPoints } from '../../apiUtils/apiEndPoints';
@@ -17,7 +18,7 @@ export function SignUp() {
     const onSubmit = async (e) => {
         e.preventDefault();
         const result = await postByApiEndpoint(userEndPoints.signUp, formData);
-        console.log(result)
+        console.log(result);
     };
 
     const handleChange = (e) => {
@@ -27,22 +28,37 @@ export function SignUp() {
 
     return (
         <div>
-            <h1>Sign Up with username and password.</h1>
+            <h2>Sign Up with username and password.</h2>
             <form onSubmit={onSubmit}>
-                <label>Name: </label>
-                <input type='text' name='name' onChange={handleChange} value={name}></input>
-
-                <label>User Name</label>
-                <input type='text' name='username' onChange={handleChange} value={username}></input>
-
-                <label>Password</label>
-                <input type='text' name='password' onChange={handleChange} value={password}></input>
-
-                <label>Confirm Password</label>
-                <input type='text' name='confirmPassword' onChange={handleChange} value={confirmPassword}></input>
-
-                <label>E-mail</label>
-                <input type='text' name='email' onChange={handleChange} value={email}></input>
+                <FormInput label={'Name'} name={'name'} onChange={handleChange} value={name} type='text' />
+                <FormInput
+                    label={'User Name'}
+                    name={'username'}
+                    onChange={handleChange}
+                    value={username}
+                    type='text'
+                />
+                <FormInput
+                    label={'Password'}
+                    name={'password'}
+                    onChange={handleChange}
+                    value={password}
+                    type='password'
+                />
+                <FormInput
+                    label={'Password Confirmation'}
+                    name={'conformPassword'}
+                    onChange={handleChange}
+                    value={confirmPassword}
+                    type='password'
+                />
+                <FormInput
+                    label={'E-mail address'}
+                    name={'email'}
+                    onChange={handleChange}
+                    value={email}
+                    type='text'
+                />
 
                 <button type='submit'>Sign Up</button>
             </form>
